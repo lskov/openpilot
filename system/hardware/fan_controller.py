37fcdb7ff12b115bacdde0c7f3536f22e76af59a
Lasse Skov <lasse.skov@gmail.com>
2025-06-06 16:28:37 +0000

    Updated PID params

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -18,7 +18,7 @@ class TiciFanController(BaseFanController):
     cloudlog.info("Setting up TICI fan handler")
 
     self.last_ignition = False
-    self.controller = PIDController(k_p=7.0, k_i=1e-3, k_f=0.5, rate=(1 / DT_HW)) 
+    self.controller = PIDController(k_p=0.1, k_i=1e-3, k_f=0.5, rate=(1 / DT_HW)) 
                     # PIDController(k_p=0,   k_i=4e-3, k_f=1,   rate=(1 / DT_HW))
 
   def update(self, cur_temp: float, ignition: bool) -> int:
